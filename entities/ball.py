28ac5452e177245e57c482f4c1846b34bec49bca
Eren Akkoç <ern.akkc@gmail.com>
2026-01-23 04:27:43 +0300

    First runnable version

--- entities/ball.py
+++ entities/ball.py
@@ -7,15 +7,16 @@ class Ball(BaseEntity):
         super().__init__(space)
         self.body = pymunk.Body(BALL_MASS, pymunk.moment_for_circle(BALL_MASS, 0, BALL_SIZE)) 
         self.body.position = x, y
-        self.shape.friction = BALL_FRICTION
+        
         self.color = (255, 255, 255)  # Default Beyaz renk
         self.name = "unnamed_ball" # Varsayılan isim
         self.photo = None  # Fotoğraf için yer tutucu
         self.sound = None  # Ses için yer tutucu
         self.body.mass = BALL_MASS
         self.size = BALL_SIZE
-        self.shape = pymunk.Circle(self.body, self.size) 
 
+        self.shape = pymunk.Circle(self.body, self.size) 
+        self.shape.friction = BALL_FRICTION
 
         space.add(self.body, self.shape)
     
